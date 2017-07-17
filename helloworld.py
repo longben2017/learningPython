58fb96f06be205c5c32724c722f007957b31dea0
longben2017 <longben2017@DEEP-1706302223>
2017-07-17 22:11:15 +0800

    merge

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+print 'hello world'
+
+print'i am a','man'
+
+print'100 + 200 =',100+200
+
+print '------'
+
+edward=['Edward Gumby',42]
+john=['John Smith',50]
+database=[edward,john]
+print database
+
+print '------'
+
+greeting='Hello'
+print greeting[0]
+print greeting[-1]
+
+print 'Hello'[1]
+
+fourth = raw_input('Year: ')[3]
+print fourth
