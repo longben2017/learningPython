bd4dd55d28fc091d78d67e2a190750d7a38b17d5
longben2017 <longben2017@DEEP-1706302223>
2017-08-06 15:32:26 +0800

    project2:draw a 'hello world' image and output with a pdf file.

---
+++ python2/project2_drawing/hello_report.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding:utf-8
+
+#hello_report.py
+
+from reportlab.graphics.shapes import Drawing,String
+from reportlab.graphics import renderPDF
+
+d = Drawing(100,100)
+s = String(50,50,'Hello,world!',textAnchor='middle')
+
+d.add(s)
+
+renderPDF.drawToFile(d,'hello.pdf','A simple PDF file')
